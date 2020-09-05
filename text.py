1d1da7932785abdaab0b949c63b5dbe8e5b5dea3
Adithya <66945553+adithyaz@users.noreply.github.com>
2020-09-05 22:04:52 +0700

    Delete text.py
    Delete text.py

--- text.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-from tkinter import messagebox
-from tkinter.filedialog import asksaveasfilename
-from tkinter import filedialog
-import os
-from tkinter.messagebox import askokcancel
